08414edd855fdcfd061e74b2416f6714ed2a9b71
kariray <kari_578@naver.com>
2023-11-19 21:40:19 +0900

    useSearchParams

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,15 @@
 import { users } from "../db";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 function Home() {
+  const [readSearchParams, setSearchParams] = useSearchParams();
+  console.log(readSearchParams.has("ray"));
+  const changeURL = () => {
+    setSearchParams({
+      day: "today",
+      tomorrow: "123",
+    });
+  };
   return (
     <div>
       <h1>Users</h1>
@@ -12,6 +20,7 @@ function Home() {
           </li>
         ))}
       </ul>
+      <button onClick={changeURL}>Change URL</button>
     </div>
   );
 }

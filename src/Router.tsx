d52e55eda8ae56733f8b471f277890a6627fb05b
kariray <kari_578@naver.com>
2023-11-19 21:05:52 +0900

    useParams

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./components/NotFound";
 import ErrorElement from "./components/ErrorElement";
+import User from "./pages/users/User";
 
 const router = createBrowserRouter([
   {
@@ -18,6 +19,10 @@ const router = createBrowserRouter([
       {
         path: "about",
         element: <About />,
+      },
+      {
+        path: "users/:userId",
+        element: <User />,
       },
     ],
     errorElement: <NotFound />,
---
+++ src/db.ts
@@ -0,0 +1,10 @@
+export const users = [
+  {
+    id: 1,
+    name: "ray",
+  },
+  {
+    id: 2,
+    name: "dog",
+  },
+];
